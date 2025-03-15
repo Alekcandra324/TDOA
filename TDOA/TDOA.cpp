ea5eed64c5a06d2974866a39df20e450e8a43467
Alekcandra324 <alekcandralykinova@gmail.com>
2025-03-15 22:15:59 +0300

    сделала так, что шаг изменения для каждой координаты - отдельное значения.Добавила циклы, чтобы они изменялись.

--- TDOA/TDOA.cpp
+++ TDOA/TDOA.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <Eigen/Dense>
+#include <unordered_map>
 using namespace std;
 using namespace Eigen;
 //мне даны координаты точек D,F,E и разность ходов ( всего их 9 значений для каждой точки соот A,B,C)
@@ -31,39 +32,71 @@ double loss_function(Vector2d A, Vector2d B, Vector2d C, const vector<Vector2d>&
 
 void Gradient_spusk(Vector2d& A, Vector2d& B, Vector2d& C, const vector<Vector2d>& DEF_points, const vector<double>& distance_diff, double step, int iterations) {
     double accuracy = 0.01;
+    
     //предыдущий код работал неправильно, хотя математически мне казалось я реализовала его верно, однако ошибка с каждой итерацией росла.
     // необходимо было внимательнее читать википедию
     // буду реализовать Метод покоординатного спуска Гаусса — Зейделя
     //для этого я буду искать каждую координату поотдельности.
     for (int iter = 0; iter < iterations; iter++) {
+        unordered_map<Vector2d*, array<double, 2>> step_sizes = {
+    { &A, {step, step} },
+    { &B, {step, step} },
+    { &C, {step, step} }//добавила отдельные шаги
+
+        };
+       
         for (int coord = 0; coord < 2; coord++) { // 0 - x, 1 - y
             for (Vector2d* point : { &A, &B, &C }) {
                 double orig_value = (*point)[coord];  // Запоминаем старое значение
                 double grad = 0.0;
+                
+
                 // сложность, поняла из этой версии , что шаг итерации для каждой координаты должен быть отдельный и меняться.Иначе то в минус уходит , то начинает уходить от правильного ответа
-                //буду думать как это объяснить машине
+               
                 for (int i = 0; i < 3; i++) {
                     Vector2d DEF = DEF_points[i];
                     double normA_DEF = (A - DEF).norm();
                     double normB_DEF = (B - DEF).norm();
                     double normC_DEF = (C - DEF).norm();
-
-                    if (normA_DEF > 1e-8 && normB_DEF > 1e-8 && normC_DEF > 1e-8) {
-                        if (point == &A) {
-                            grad += 2 * ((normA_DEF - normB_DEF - distance_diff[i * 3]) * ((A - DEF)[coord] / normA_DEF) +
-                                (normA_DEF - normC_DEF - distance_diff[i * 3 + 1]) * ((A - DEF)[coord] / normA_DEF));
-                        }
-                        if (point == &B) {
-                            grad += 2 * ((normB_DEF - normC_DEF - distance_diff[i * 3 + 2]) * ((B - DEF)[coord] / normB_DEF) -
-                                (normA_DEF - normB_DEF - distance_diff[i * 3]) * ((B - DEF)[coord] / normB_DEF));
-                        }
-                        if (point == &C) {
-                            grad += 2 * (-(normA_DEF - normC_DEF - distance_diff[i * 3 + 1]) * ((C - DEF)[coord] / normC_DEF) -
-                                (normB_DEF - normC_DEF - distance_diff[i * 3 + 2]) * ((C - DEF)[coord] / normC_DEF));
-                        }
+                    if (point == &A) {
+                        grad += 2 * ((normA_DEF - normB_DEF - distance_diff[i * 3]) * ((A - DEF)[coord] / normA_DEF) +
+                            (normA_DEF - normC_DEF - distance_diff[i * 3 + 1]) * ((A - DEF)[coord] / normA_DEF));
                     }
+                    if (point == &B) {
+                        grad += 2 * ((normB_DEF - normC_DEF - distance_diff[i * 3 + 2]) * ((B - DEF)[coord] / normB_DEF) -
+                            (normA_DEF - normB_DEF - distance_diff[i * 3]) * ((B - DEF)[coord] / normB_DEF));
+                    }
+                    if (point == &C) {
+                        grad += 2 * ((normA_DEF - normC_DEF - distance_diff[i * 3 + 1]) * ((C - DEF)[coord] / normC_DEF) -
+                            (normB_DEF - normC_DEF - distance_diff[i * 3 + 2]) * ((C - DEF)[coord] / normC_DEF));
+                    }
+
+                   
+                    
                 }
-                (*point)[coord] -= step * grad;
+                double cost = loss_function(A, B, C, DEF_points, distance_diff);
+                (*point)[coord] -= step_sizes[point][coord] * grad;
+                double new_cost = loss_function(A, B, C, DEF_points, distance_diff);
+                if (new_cost - cost > 0) {
+
+                    while (new_cost - cost > 0) {
+                       
+                        (*point)[coord] += step_sizes[point][coord] * grad;
+                        step_sizes[point][coord] *= 0.01;
+                        (*point)[coord] -= step_sizes[point][coord] * grad;
+                        
+                        new_cost = loss_function(A, B, C, DEF_points, distance_diff);
+                        
+
+                    }
+
+
+                }
+                else  step_sizes[point][coord] *= 1.5;
+                cout << iter << "\n";
+                
+                
+                
             }
         }
     }
@@ -85,14 +118,15 @@ int main() {
     vector<double> distance_diff = { 5.78,20.89 , 15.11, 5.71, 19.75, 14.04, 5.70,21.45 , 15.75 };
 
     // Начальные догадки
-    Vector2d A(10, 10), B(10, 20), C(20, 10);
+    Vector2d A(10, 10), B(10, 10), C(10, 25);
 
     // Запускаем градиентный спуск
-    Gradient_spusk(A, B, C, DEF_points, distance_diff, 0.0001, 10000);
+    Gradient_spusk(A, B, C, DEF_points, distance_diff, 1, 10000);
 
     // Вывод результата
     cout << "A: (" << A.x() << ", " << A.y() << ")\n";
     cout << "B: (" << B.x() << ", " << B.y() << ")\n";
     cout << "C: (" << C.x() << ", " << C.y() << ")\n";
     return 0;
+    //A(0,0), B(3,5), C(8,20) 
 }
